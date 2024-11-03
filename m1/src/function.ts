2b0ee38f8c5c5dccf3e8a6e5dcb428d33a1f5e32
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-03 10:02:40 +0600

    funcion type

---
+++ m1/src/function.ts
@@ -0,0 +1,13 @@
+{
+    function add(num1: number, num2: number):number{
+        return num1 + num2;
+    }
+    add(23, 27);
+    const add2 =(num1:number, num2:number):number=> num1 +  num2;
+    
+    const price =(product1:number, product2:number) :number =>{
+        const total = product1 + product2;
+        return total;
+    }
+    price(234, 186);
+}
